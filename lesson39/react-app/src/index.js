6dc129421054a7421023420b4ba9ef2b469c894e
alexcodaline <aleksander.ponom@gmail.com>
2023-09-05 13:17:05 +0300

    lesson39 init local server

--- lesson39/react-app/src/index.js
+++ lesson39/react-app/src/index.js
@@ -1,2 +1,4 @@
+import './scss/style.scss'
+
 let root = document.getElementById('root');
 root.innerHTML = 'Webpack is working';
--- lesson39/react-app/webpack.config.js
+++ lesson39/react-app/webpack.config.js
@@ -1,17 +1,31 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TreserPlugin = require("terser-webpack-plugin");
+const isProd = process.env.NODE_ENV === "production";
 module.exports = {
-    entry: './src/index.js',
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[hash].js'
-    },  
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './src/index.html'
-        }),
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].js",
+  },
+  plugins: [
+    new HTMLWebpackPlugin({ template: "./src/index.html" }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "[name].[hash].css" }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ],
-}
-
+  },
+  optimization: {
+    minimizer: isProd ? [new CssMinimizerPlugin(), new TreserPlugin()] : [],
+  },
+};
