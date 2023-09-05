9800df65c5c7299f56583b2bb56e654f0038951d
alexcodaline <aleksander.ponom@gmail.com>
2023-09-05 13:35:21 +0300

    lesson39 Webpack

--- lesson39/react-app/src/index.js
+++ lesson39/react-app/src/index.js
@@ -1,4 +1,8 @@
 import './scss/style.scss'
+import App from './App.js'
+import React from 'react'
+import * as ReactDOMClient from 'react-dom/client';
 
-let root = document.getElementById('root');
-root.innerHTML = 'Webpack is working';
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
+root.render(<App />);
