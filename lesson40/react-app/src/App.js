4aaff3c15fe3a8b6b3fc848474e57eea9125d3f9
alexcodaline <aleksander.ponom@gmail.com>
2023-09-05 13:59:06 +0300

    lesson40 init

---
+++ lesson40/react-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Article from "./components/Article";
+import ArticleBody from "./components/ArticleBody";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1 className="title">NVIDIA news</h1>
+      <div className="article">
+        <Article />
+        <ArticleBody />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ lesson40/react-app/src/components/ArticleBody.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ArticleBody() {
+  return (
+    <div className="article__title">
+      <h3>Article description:</h3>
+      <p>
+        NVIDIA on Azure is bringing AI, networking, and hight-perfomance
+        computing to the enterprise.
+      </p>
+    </div>
+  );
+}
+export default ArticleBody;
